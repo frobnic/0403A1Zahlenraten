3958e79986eae32a50a3d9323a0144dfda8fc8c3
frank paulsen <frobnic+ita@gmail.com>
2019-08-06 11:11:07 +0200

    code inserted

--- 0403A1Zahlenraten/Program.cs
+++ 0403A1Zahlenraten/Program.cs
@@ -10,6 +10,25 @@ namespace _0403A1Zahlenraten
     {
         static void Main(string[] args)
         {
+            int z = 0; // Später mal, Zufallszahl
+            int n = 0; // Zähler für Versuche
+            int r = 0; // Ratezahl
+
+            Random zufall = new Random();
+            z = zufall.Next(1, 101);
+
+            do
+            {
+                Console.WriteLine("yessir?");
+                r = int.Parse(Console.ReadLine());
+                if (r > z)
+                    Console.WriteLine("zu hoch");
+                if (r < z)
+                    Console.WriteLine("zu tief");
+                n++;
+            } while (r != z);
+            Console.WriteLine("nach {0} versuchen gefunden!", n);
+
         }
     }
 }
